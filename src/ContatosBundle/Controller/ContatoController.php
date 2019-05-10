d1af2fbe8d9e065cef9a9c22bbba725627869037
filipebsmonteiro <filipebsmonteiro@gmail.com>
2019-05-10 16:08:16 -0300

    Ajustes pela IDE

--- src/ContatosBundle/Controller/ContatoController.php
+++ src/ContatosBundle/Controller/ContatoController.php
@@ -3,6 +3,8 @@
 namespace ContatosBundle\Controller;
 
 use ContatosBundle\Entity\Contato;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
